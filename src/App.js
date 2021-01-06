df1e44814bfb228b42ea61f2125c32295eebe887
SoniaDumitru <sonya_dumitru@yahoo.com>
2021-01-06 15:30:29 -0600

    finalized graph and implemented select from react-select library

--- src/App.js
+++ src/App.js
@@ -1,48 +1,114 @@
+import React, { useState, useEffect } from 'react';
+
+// Styling
 import './App.css';
-import React, { useState, useEffect }  from 'react';
+
+// Components
+import Graph from './components/Graph';
+import Select from 'react-select';
 
 function App() {
+
   const [data, setData] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [options, setOptions] = useState([])
+  const [firstLabel, setFirstLable] = useState('US');
+  const [secondLable, setSecondLable] = useState('ROMANIA');
+  const [firstSelect, setFirstSelect] = useState(212);
+  const [secondSelect, setSecondSelect] = useState(249);
 
+  // fetch data
+  const runCall = async () => {
+    let apiValue = await fetchData();
+    const header = apiValue.split('\n').slice(0).map(line => (line.split(',')))[0].slice(4);
+
+    const firstPrep = apiValue.split('\n').slice(0).map(line => (line.split(',')))[firstSelect]
+    const secondPrep = apiValue.split('\n').slice(0).map(line => (line.split(',')))[secondSelect]
+
+    const firstLbl = firstPrep[1].toUpperCase();
+    const secondLbl = secondPrep[1].toUpperCase();
+
+    const formatData = [];
+    for (let i = 4; i < header.length; i++) {
+      const newRow = {};
+      newRow.name = header[i];
+      newRow[firstLbl] = parseInt(firstPrep[i]);
+      newRow[secondLbl] = parseInt(secondPrep[i]);
+      formatData.push(newRow);
+    }
+
+    const countriesList = apiValue.split('\n').slice(1).map(line => (line.split(',').slice(0, 2)));
+    console.log(countriesList)
+    const countriesArr = [];
+    for (let i = 0; i < countriesList.length; i++) {
+      countriesArr.push({ label: countriesList[i].reverse().join(' '), value: i + 1 })
+    }
+
+    setFirstLable(firstLbl);
+    setSecondLable(secondLbl);
+    setOptions(countriesArr)
+    setData(formatData);
+    setIsLoading(false);
+    return null;
+  }
+
+  // inner helper fetch function
+  const fetchData = async () => {
+    const requestOption = {
+      method: "GET",
+      redirect: "follow"
+    }
+
+    const apiUrl = `https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv`
+
+    try {
+      const response = await fetch(apiUrl, requestOption);
+      return response.ok ? response.text() : null
+    } catch (err) {
+      console.log(err);
+      return null;
+    }
+  }
+
+  // calls api everytime select changes 
   useEffect(() => {
-      const runCall = async () => {
-          let apiValue = await fetchData();
-          const header = apiValue.split('\n').slice(0).map(line => (line.split(',')))[0].slice(4);
-          const usaData = apiValue.split('\n').slice(0).map((line) => line.split(','))[226].slice(4)
-          const charData = [];
-          for (let i = 0; i < header.length; i++) {
-            const newRow = {};
-            newRow.name = header[i];
-            newRow.data = parseInt(usaData[i]);
-            charData.push(newRow);
-          }
-          setData(charData);
-          setIsLoading(false);
-          return null;
-      }
-      const fetchData = async () => {
-          const requestOption = {
-              method: 'GET',
-              redirect: 'follow'
-          }
-          const apiUrl = `https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv`
-          try {
-              const response = await fetch(apiUrl, requestOption);
-              return response.ok ? response.text() : null;
-          } catch (err) {
-              console.log(err);
-              return null;
-          }
-      }
-      runCall();
-  }, [])
+    runCall();
+  }, [firstSelect, secondSelect])
+
+  const firstInput = (input) => {
+    setFirstSelect(input.value)
+  }
+
+  const secondInput = (input) => {
+    setSecondSelect(input.value)
+  }
 
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div className="app-wrap">
+      <h1>COVID-19 Country Comparator</h1>
+      {isLoading ?
+        <div>loading...</div> :
+        <div className="content-wrap">
+          <Graph 
+            data={data} 
+            label_1={firstLabel} 
+            label_2={secondLable} 
+          />
+          <div className="selector-wrap">
+            <Select 
+              options={options} 
+              onChange={firstInput} 
+              placeholder="ROMANIA" 
+            />
+            <Select 
+              options={options} 
+              onChange={secondInput} 
+              placeholder="US" 
+            />
+          </div>
+        </div>
+      }
     </div>
   );
 }
-
 export default App;
--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,17 +1,41 @@
-import './App.css';
-import React from 'react';
+import React from 'react'
 import {
     LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
 } from 'recharts';
 
-function Graph() {
 
-
-  return (
-    <div className="App">
-      <h1>Charts</h1>
-    </div>
-  );
+function Graph(props) {
+    return (
+        <div className="content-wrap">
+            <LineChart
+                width={800}
+                height={500}
+                data={props.data}
+                margin={{
+                    top: 5, right: 30, left: 20, bottom: 5,
+                }}
+            >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis/>
+                <Tooltip/>
+                <Legend/>
+                <Line 
+                    type="monotone" 
+                    dataKey={props.label_1} 
+                    stroke="#8884d8" 
+                    activeDot={{ r: 8 }} 
+                />
+                <Line 
+                    type="monotone" 
+                    dataKey={props.label_2} 
+                    stroke="#87d110" 
+                    activeDot={{ r: 8 }} 
+                />
+            </LineChart>
+        </div>
+    )
 }
+
 
 export default Graph;
