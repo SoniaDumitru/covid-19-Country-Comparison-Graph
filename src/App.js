2792a670f0a783245dff63900b361ed3ec29456c
SoniaDumitru <sonya_dumitru@yahoo.com>
2021-01-06 13:55:45 -0600

    construct data chart object

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import './App.css';
 import React, { useState, useEffect }  from 'react';
 
 function App() {
-  const [data, setDate] = useState(null);
+  const [data, setData] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
       const runCall = async () => {
           let apiValue = await fetchData();
-          console.log('hsjakhak', apiValue)
+          const header = apiValue.split('\n').slice(0).map(line => (line.split(',')))[0].slice(4);
+          const usaData = apiValue.split('\n').slice(0).map((line) => line.split(','))[226].slice(4)
+          const charData = [];
+          for (let i = 0; i < header.length; i++) {
+            const newRow = {};
+            newRow.name = header[i];
+            newRow.data = parseInt(usaData[i]);
+            charData.push(newRow);
+          }
+          setData(charData);
+          setIsLoading(false);
+          return null;
       }
       const fetchData = async () => {
           const requestOption = {
--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,12 +1,15 @@
 import './App.css';
 import React from 'react';
+import {
+    LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
+} from 'recharts';
 
 function Graph() {
 
 
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>Charts</h1>
     </div>
   );
 }
