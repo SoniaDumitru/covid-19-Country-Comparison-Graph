14d47e30bedd57f11c9bc2e03c410571034888b7
SoniaDumitru <sonya_dumitru@yahoo.com>
2021-01-06 12:03:25 -0600

    created useEffect to fetch data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect }  from 'react';
 
 function App() {
+  const [data, setDate] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+      const runCall = async () => {
+          let apiValue = await fetchData();
+          console.log('hsjakhak', apiValue)
+      }
+      const fetchData = async () => {
+          const requestOption = {
+              method: 'GET',
+              redirect: 'follow'
+          }
+          const apiUrl = `https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv`
+          try {
+              const response = await fetch(apiUrl, requestOption);
+              return response.ok ? response.text() : null;
+          } catch (err) {
+              console.log(err);
+              return null;
+          }
+      }
+      runCall();
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
     </div>
   );
 }
---
+++ src/components/Graph.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+
+function Graph() {
+
+
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default Graph;
