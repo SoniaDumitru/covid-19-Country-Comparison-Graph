d725763f189b74de41150245ebcb42007f41abe9
SoniaDumitru <sonya_dumitru@yahoo.com>
2021-01-11 10:57:54 -0600

    Updates

---
+++ precache-manifest.03a13673093f2370fdd2f36f0e09ec0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "558b5f544dc6cf847a7ed60120f68f24",
+    "url": "/SoniaDumitru/covid-19-country-comparison-graph/index.html"
+  },
+  {
+    "revision": "75475a98fc29891e6826",
+    "url": "/SoniaDumitru/covid-19-country-comparison-graph/static/css/main.b038e841.chunk.css"
+  },
+  {
+    "revision": "a8c94db6a15254aeb876",
+    "url": "/SoniaDumitru/covid-19-country-comparison-graph/static/js/2.081ab955.chunk.js"
+  },
+  {
+    "revision": "f3e91ced0874a3e507ef0e80f41dde1e",
+    "url": "/SoniaDumitru/covid-19-country-comparison-graph/static/js/2.081ab955.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75475a98fc29891e6826",
+    "url": "/SoniaDumitru/covid-19-country-comparison-graph/static/js/main.d224db74.chunk.js"
+  },
+  {
+    "revision": "e7810534001caf980de0",
+    "url": "/SoniaDumitru/covid-19-country-comparison-graph/static/js/runtime-main.cdbc8bec.js"
+  }
+]);
